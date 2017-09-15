39cb47a1dfb51bda79157664c3d58085853758d3
Fabian Poels <fabian.poels@gmail.com>
2017-09-15 22:09:49 +0200

    pugshizzle

--- gulpfile.js
+++ gulpfile.js
@@ -9,52 +9,76 @@ var htmlmin = require('gulp-htmlmin');
 var imagemin = require('gulp-imagemin');
 var pug = require('gulp-pug');
 
-gulp.task('optimize', function(){
-  return gulp.src('src/*.html')
-    .pipe(useref())
-    .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulpIf('*.css', cssnano()))
-    .pipe(gulpIf('*.html', htmlmin({collapseWhitespace: true})))
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('analyticsJs', function(){
-  return gulp.src('src/js/googleAnalytics.js')
-    .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulp.dest('dist/js'));
-})
-
-gulp.task('photos', function(){
-  return gulp.src('src/photos/*')
-    .pipe(gulp.dest('dist/photos'));;
-});
-
-gulp.task('assets', function(){
-  return gulp.src('src/assets/**/*.+(png|svg)')
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/assets'));
-});
-
-gulp.task('clean:dist', function(){
-  return del.sync('dist');
-});
-
-gulp.task('buildHTML', function buildHTML() {
-  return gulp.src('*.pug')
-    .pipe(pug({})
-    .pipe(gulp.dest('dist'))
-  );
-});
-
-gulp.task('pugtest', function() {
+//pug to dist tasks
+gulp.task('dist:html', function() {
   return gulp.src('src/*.pug')
     .pipe(pug({
       doctype: 'html',
       pretty: true
     }))
-    .pipe(gulp.dest('pugtest'));
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', function(callback){
-  runSequence('clean:dist', ['optimize', 'analyticsJs','photos', 'assets'], callback);
+gulp.task('dist:assets', function(){
+  return gulp.src('src/assets/**/*.+(png|svg)')
+    .pipe(gulp.dest('dist/assets'));
+});
+
+gulp.task('dist:photos', function(){
+  return gulp.src('src/photos/*')
+    .pipe(gulp.dest('dist/photos'));;
+});
+
+gulp.task('dist:css', function(){
+  return gulp.src('src/css/*')
+    .pipe(gulp.dest('dist/css'));;
+});
+
+gulp.task('dist:js', function(){
+  return gulp.src('src/js/*')
+    .pipe(gulp.dest('dist/js'));;
+});
+
+gulp.task('dist:clean', function(){
+  return del.sync('dist');
+});
+
+gulp.task('dist', function(callback){
+  runSequence('dist:clean', ['dist:html', 'dist:assets', 'dist:photos', 'dist:css', 'dist:js'], callback);
+});
+
+//dist to production tasks
+
+gulp.task('optimize', function(){
+  return gulp.src('dist/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulpIf('*.html', htmlmin({collapseWhitespace: true})))
+    .pipe(gulp.dest('production'));
+});
+
+gulp.task('production:analyticsJs', function(){
+  return gulp.src('dist/js/googleAnalytics.js')
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('production/js'));
 })
+
+gulp.task('photos', function(){
+  return gulp.src('dist/photos/*')
+    .pipe(gulp.dest('production/photos'));;
+});
+
+gulp.task('assets', function(){
+  return gulp.src('dist/assets/**/*.+(png|svg)')
+    .pipe(imagemin())
+    .pipe(gulp.dest('production/assets'));
+});
+
+gulp.task('production:clean', function(){
+  return del.sync('production');
+});
+
+gulp.task('production', function(callback){
+  runSequence('production:clean', ['optimize', 'production:analyticsJs','photos', 'assets'], callback);
+});
