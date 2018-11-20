32c8111ab1cff528ca36570183ee3b2b4718f7cc
Fabian <fabian.poels@gmail.com>
2018-11-20 13:20:50 +0100

    update: font, portrait, germany

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ let app = express()
 
 app.use(express.static('production'))
 
-let server = app.listen (8080, () => {
+let server = app.listen (8081, () => {
   console.log('FABIANPOELS.COM running at localhost:' + server.address().port)
 })
