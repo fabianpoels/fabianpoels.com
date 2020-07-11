af174bae24688556d525eb326cdf3316daad83e9
Fabian <fabian.poels@gmail.com>
2020-07-11 14:39:54 +0200

    cover update

--- src/js/background.js
+++ src/js/background.js
@@ -1,25 +1,45 @@
-var landscapeImages = ['fabianpoels_latelier_flo.jpg', 'fabianpoels_rienztal.jpg', 'fabianpoels_yves_freyr_manhattantransfer.jpg', 'fabianpoels_lesimoust.jpg']
-var portraitImages = ['fabianpoels_ciastlins.jpg', 'fabianpoels_ailefroide_boulder.jpg', 'fabianpoels_matthiasschiestl_stohlwond_bockhaus.jpg', 'fabianpoels_lesimoust_02.jpg']
+var landscapeImages = [
+  'fabianpoels_latelier_flo.jpg',
+  'fabianpoels_yves_freyr_manhattantransfer.jpg',
+  'fabianpoels_landscape_04.jpg',
+  'fabianpoels_landscape_03.jpg',
+]
+var portraitImages = [
+  'fabianpoels_ciastlins.jpg',
+  'fabianpoels_ailefroide_boulder.jpg',
+  'fabianpoels_matthiasschiestl_stohlwond_bockhaus.jpg',
+  'fabianpoels_portrait_10.jpg',
+]
 var mq = window.matchMedia('(max-width: 1080px)')
 setBackground(mq)
 mq.addListener(setBackground)
 
-function setBackground (mq) {
+function setBackground(mq) {
   if (mq.matches) {
     $('body').css({
-      'background': 'url(images/covers/' + portraitImages[Math.floor(Math.random() * Math.floor(portraitImages.length))] + ') no-repeat center center fixed',
+      background:
+        'url(images/covers/' +
+        portraitImages[
+          Math.floor(Math.random() * Math.floor(portraitImages.length))
+        ] +
+        ') no-repeat center center fixed',
       '-webkit-background-size': 'cover',
       '-moz-background-size': 'cover',
       '-o-background-size': 'cover',
-      'background-size': 'cover'
+      'background-size': 'cover',
     })
   } else {
     $('body').css({
-      'background': 'url(images/covers/' + landscapeImages[Math.floor(Math.random() * Math.floor(landscapeImages.length))] + ') no-repeat center center fixed',
+      background:
+        'url(images/covers/' +
+        landscapeImages[
+          Math.floor(Math.random() * Math.floor(landscapeImages.length))
+        ] +
+        ') no-repeat center center fixed',
       '-webkit-background-size': 'cover',
       '-moz-background-size': 'cover',
       '-o-background-size': 'cover',
-      'background-size': 'cover'
+      'background-size': 'cover',
     })
   }
 }
