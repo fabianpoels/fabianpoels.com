01367fccd41780943ff5ed89a3fa89888c3e92a5
Fabian <fabian.poels@gmail.com>
2022-04-11 09:26:50 +0200

    onhold version

--- src/js/background.js
+++ src/js/background.js
@@ -1,45 +1,14 @@
-var landscapeImages = [
-  'fabianpoels_latelier_flo.jpg',
-  'fabianpoels_yves_freyr_manhattantransfer.jpg',
-  'fabianpoels_landscape_04.jpg',
-  'fabianpoels_landscape_03.jpg',
-]
-var portraitImages = [
-  'fabianpoels_ciastlins.jpg',
-  'fabianpoels_ailefroide_boulder.jpg',
-  'fabianpoels_matthiasschiestl_stohlwond_bockhaus.jpg',
-  'fabianpoels_portrait_10.jpg',
-]
-var mq = window.matchMedia('(max-width: 1080px)')
-setBackground(mq)
-mq.addListener(setBackground)
+var landscapeImages = ['fabianpoels_cover_image_mountain.jpg']
+var portraitImages = []
+setBackground()
 
-function setBackground(mq) {
-  if (mq.matches) {
-    $('body').css({
-      background:
-        'url(images/covers/' +
-        portraitImages[
-          Math.floor(Math.random() * Math.floor(portraitImages.length))
-        ] +
-        ') no-repeat center center fixed',
+function setBackground() {
+  $('body').css({
+    background:
+      'url(images/covers/fabianpoels_cover_image_mountain.jpg) no-repeat center center fixed',
       '-webkit-background-size': 'cover',
       '-moz-background-size': 'cover',
       '-o-background-size': 'cover',
       'background-size': 'cover',
-    })
-  } else {
-    $('body').css({
-      background:
-        'url(images/covers/' +
-        landscapeImages[
-          Math.floor(Math.random() * Math.floor(landscapeImages.length))
-        ] +
-        ') no-repeat center center fixed',
-      '-webkit-background-size': 'cover',
-      '-moz-background-size': 'cover',
-      '-o-background-size': 'cover',
-      'background-size': 'cover',
-    })
-  }
+  })
 }
