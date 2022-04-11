b3177e458978603042c244eedcb27a7bd76b3957
Fabian <fabian.poels@gmail.com>
2022-04-11 10:46:00 +0200

    image update

--- src/js/background.js
+++ src/js/background.js
@@ -1,11 +1,11 @@
-var landscapeImages = ['fabianpoels_cover_image_mountain.jpg']
+var landscapeImages = ['fabianpoels_cover_image_mountain_hq.jpg']
 var portraitImages = []
 setBackground()
 
 function setBackground() {
   $('body').css({
     background:
-      'url(images/covers/fabianpoels_cover_image_mountain.jpg) no-repeat center center fixed',
+      'url(images/covers/fabianpoels_cover_image_mountain_hq.jpg) no-repeat center center fixed',
       '-webkit-background-size': 'cover',
       '-moz-background-size': 'cover',
       '-o-background-size': 'cover',
