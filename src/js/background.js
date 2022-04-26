e0b8340bc1524db19630bd35b9431789838cc198
Fabian <fabian.poels@gmail.com>
2022-04-26 12:42:25 +0200

    background image update

--- src/js/background.js
+++ src/js/background.js
@@ -1,11 +1,12 @@
-var landscapeImages = ['fabianpoels_cover_image_mountain_hq.jpg']
+var landscapeImages = ['fabianpoels_cover_image_mountain_hq.jpg', 'fabianpoels_cover_image_mountain_hq_2.jpg']
 var portraitImages = []
 setBackground()
 
 function setBackground() {
+  const image_url = landscapeImages[Math.floor(Math.random() * Math.floor(landscapeImages.length))]
   $('body').css({
     background:
-      'url(images/covers/fabianpoels_cover_image_mountain_hq.jpg) no-repeat center center fixed',
+      `url(images/covers/${image_url}) no-repeat center center fixed`,
       '-webkit-background-size': 'cover',
       '-moz-background-size': 'cover',
       '-o-background-size': 'cover',
