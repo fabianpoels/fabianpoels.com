927eae8b3539a0fb5b54ff2597e2ee398f5b586e
Fabian <fabian.poels@gmail.com>
2022-10-14 12:56:11 +0200

    update

--- src/js/background.js
+++ src/js/background.js
@@ -1,20 +1,27 @@
 var landscapeImages = [
-  'fabianpoels_cover_image_mountain_hq.jpg',
+  'fabianpoels_cover_image_mountain_hq_1.jpg',
   'fabianpoels_cover_image_mountain_hq_2.jpg',
   'fabianpoels_cover_image_mountain_hq_3.jpg',
-  'fabianpoels_cover_image_mountain_hq_4.jpg'
+  'fabianpoels_cover_image_mountain_hq_4.jpg',
+  'fabianpoels_cover_image_mountain_hq_5.jpg',
 ]
 var portraitImages = []
-setBackground()
+setRandomBackground()
 
-function setBackground() {
-  const image_url = landscapeImages[Math.floor(Math.random() * Math.floor(landscapeImages.length))]
+function setRandomBackground() {
+  setBackground(Math.floor(Math.random() * Math.floor(landscapeImages.length)))
+}
+
+function setBackground(index) {
+  const image_url = landscapeImages[index]
   $('body').css({
-    background:
-      `url(images/covers/${image_url}) no-repeat center center fixed`,
-      '-webkit-background-size': 'cover',
-      '-moz-background-size': 'cover',
-      '-o-background-size': 'cover',
-      'background-size': 'cover',
+    background: `url(images/covers/${image_url}) no-repeat center center fixed`,
+    '-webkit-background-size': 'cover',
+    '-moz-background-size': 'cover',
+    '-o-background-size': 'cover',
+    'background-size': 'cover',
   })
+
+  $('div.image-button').removeClass('selected')
+  $(`div#image-${index}`).addClass('selected')
 }
