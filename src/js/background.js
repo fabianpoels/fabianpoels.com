f95c7755c76388f76e103a78ae1a066607ea5b39
Fabian <fabian.poels@gmail.com>
2022-10-21 20:26:01 +0200

    images rework

--- src/js/background.js
+++ src/js/background.js
@@ -1,38 +1,26 @@
-const landscapeImages = [
-  'fabianpoels_cover_image_mountain_hq_1.jpg',
-  'fabianpoels_cover_image_mountain_hq_2.jpg',
-  'fabianpoels_cover_image_mountain_hq_3.jpg',
-  'fabianpoels_cover_image_mountain_hq_4.jpg',
-  'fabianpoels_cover_image_mountain_hq_5.jpg',
-]
-const portraitImages = []
+const images_count = 5
 let current_index
-// setRandomBackground()
+
 setBackground(0)
 
 document.addEventListener('keyup', (e) => {
   if (e.code === 'ArrowLeft' && current_index > 0) {
     setBackground(current_index - 1)
   }
-  if (e.code === 'ArrowRight' && current_index < landscapeImages.length - 1) {
+  if (e.code === 'ArrowRight' && current_index < images_count - 1) {
     setBackground(current_index + 1)
   }
 })
 
 function setRandomBackground() {
-  setBackground(Math.floor(Math.random() * Math.floor(landscapeImages.length)))
+  setBackground(Math.floor(Math.random() * Math.floor(images_count)))
 }
 
 function setBackground(index) {
   current_index = index
-  const image_url = landscapeImages[index]
-  $('body').css({
-    background: `url(images/covers/${image_url}) no-repeat center center fixed`,
-    '-webkit-background-size': 'cover',
-    '-moz-background-size': 'cover',
-    '-o-background-size': 'cover',
-    'background-size': 'cover',
-  })
+
+  $(`div.background`).removeClass('selected-background')
+  $(`div#background-${index}`).addClass('selected-background')
 
   $('div.image-button').removeClass('selected')
   $(`div#image-${index}`).addClass('selected')
