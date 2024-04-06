bfb35185a7902c6761fa3f6ce82df7a9c290011d
Fabian Poels <fabian@vertical-life.info>
2024-04-06 07:26:22 +0200

    changed image tracking

--- src/js/imagegallery.js
+++ src/js/imagegallery.js
@@ -19,8 +19,9 @@ Fancybox.bind('[data-fancybox="gallery"]', {
     'Carousel.ready Carousel.settle': (fancybox) => {
       const slide = fancybox.getSlide()
       if (slide && slide.src) {
-        gtag('event', 'view_image', {
-          'image_name': slide.src
+        const name = slide.src.split('fabianpoels_')[1].split('.jpg')[0]
+        gtag('event', `view_image_${name}`, {
+          'image_name': name
         });
       }
     },
